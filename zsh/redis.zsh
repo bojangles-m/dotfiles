e2300ffdbff6898081a98f04984d3050cd712a95
Bojch <bojan.mazej@gmail.com>
2020-10-03 12:16:08 +0200

    Redis

---
+++ zsh/redis.zsh
@@ -0,0 +1,5 @@
+alias redis-start="brew services start redis"
+alias redis-stop="brew services stop redis"
+alias redis-restart="brew services restart redis"
+alias redis-monitor="redis-cli monitor"
+alias redis-ping="redis-cli ping" # is radis up and running
