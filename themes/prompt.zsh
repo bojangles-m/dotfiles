bd1c951ea3cac41ab90016d05e79a96ce055a517
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2024-05-07 20:43:18 +0200

    updated symbols

--- themes/prompt.zsh
+++ themes/prompt.zsh
@@ -1,6 +1,6 @@
 function getRandomSymbol() {
     typeset -A LIST_OF_SYMBOLS
-    LIST_OF_SYMBOLS=(doughnut "\U1F369" popcorn "\U1F37F" tada "\U1F389" cake "\U1F370" beer "\U1F37A" beers "\U1F37B" santa "\U1F385" shrimp "\U1F364" sushi "\U1F363" waterMelon "\U1F349" downArrowStrong "\U2B07" apple "\U1F34E" bowAndArrow "\U1F3F9" upArrowStrong "\U2B06" hotdog "\U1F32D" tacos "\U1F32E" palm "\U1F334" corn "\U1F33D")
+    LIST_OF_SYMBOLS=(scissors "✂️" doughnut "\U1F369" popcorn "\U1F37F" tada "\U1F389" cake "\U1F370" beer "\U1F37A" beers "\U1F37B" santa "\U1F385" shrimp "\U1F364" sushi "\U1F363" waterMelon "\U1F349" downArrowStrong "\U2B07" apple "\U1F34E" bowAndArrow "\U1F3F9" upArrowStrong "\U2B06" hotdog "\U1F32D" tacos "\U1F32E" palm "\U1F334" corn "\U1F33D")
     LIST_OF_KEYS=($(for key value in "${(@kv)LIST_OF_SYMBOLS}"; do echo "$key"; done))
 
     MAX=${#LIST_OF_SYMBOLS[@]}
