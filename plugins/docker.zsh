6223149cecc1d7d9d98cdee294efe5b28b4eb320
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2024-05-24 16:26:26 +0200

    updated docker alias

--- plugins/docker.zsh
+++ plugins/docker.zsh
@@ -2,18 +2,15 @@
 # https://www.gnu.org/software/bc/manual/dc-1.05/html_mono/dc.html
 
 alias dco='docker compose'
-
 alias dcb='docker compose build'
-alias dce='docker compose exec'
-alias dcps='docker compose ps'
-alias dcrestart='docker compose restart'
-alias dcrm='docker compose rm'
+alias dcup='docker compose up'
+alias dcbup='docker compose up --build'
 alias dcr='docker compose run'
 alias dcstop='docker compose stop'
-alias dcup='docker compose up'
-alias dcupb='docker compose up --build'
-alias dcupd='docker compose up -d'
 alias ddown='docker compose down'
+alias dce='docker compose exec'
+alias dcrestart='docker compose restart'
+alias dcrm='docker compose rm'
 alias dcl='docker compose logs'
 alias dclf='docker compose logs -f'
 alias dcpull='docker compose pull'
@@ -21,6 +18,7 @@ alias dcstart='docker compose start'
 alias dck='docker compose kill'
 
 alias dps='docker ps -a'
-alias dimg="docker image list"
+alias dimg="docker image ls"
+alias dl='docker login --username'
 
-alias dcrenew= 'docker compose up --build --pull=always --remove-orphans --renew-anon-volumes'
+alias dcrenew='docker compose up --build --pull=always --remove-orphans --renew-anon-volumes'
