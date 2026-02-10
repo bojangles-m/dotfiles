dfe479b6ad179c16e3bbd3c99647e26f970bc186
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2026-02-10 15:54:34 +0100

    Updated projects

--- shell/plugins/project.zsh
+++ shell/plugins/project.zsh
@@ -6,11 +6,13 @@ alias idx-run='indexer php application/cli/daemons/indexer.php'
 alias idx-reindex='app-server php application/cli/search/reindex_by_account.php'
 alias idx-setup='app-server php application/cli/search/setup_index.php'
 
-alias test:w="pnpm test:unit:watch"
-
 # Deptrac: Main goal was to prevent misusage of module’s architecture.
 # deptrac.yaml is checking the structure of modules
 # deptrac-legacy.yaml how modules in legacy architecture are used.
 # You can run Deptrac locally with commands:
 alias dpt=app-server php composer.phar run deptrac
 alias dptl=app-server php composer.phar run deptrac:legacy
+
+alias test:w="pnpm test:unit:watch"
+alias test:c:w="pnpm test:coverage:watch"
+
